500fa67bbe3b5e02b274e2058919592c838520ff
seondal <sseondal@gmail.com>
2023-08-03 01:21:15 +0900

    ✨ feat :  확대금지

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body className="flex h-[100dvh] w-screen justify-center bg-slate-100 py-px">
+      <body className="flex h-[100dvh] w-screen touch-none justify-center bg-slate-100 py-px">
         <div className="h-full w-full max-w-440 bg-white text-black drop-shadow-2xl">
           <Header />
           {children}
