5086a4201ccf4a3355f6c7ec2adc624a7d43d703
Kuesung Park <gueit214@naver.com>
2023-08-02 11:42:58 +0900

    feat : tailwind design system

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2f77752236c3cc87"; }
+/******/ 		__webpack_require__.h = function() { return "4bd37816e91d9a17"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- styles/theme/colors.ts
+++ styles/theme/colors.ts
@@ -1,25 +1,7 @@
 export const colors = {
-  blue: '#1249FC',
-  blue2: '#ABC1FC',
-  blue3: '#3A6DFC',
-
-  yellow: '#FFE605',
-  orange: '#D7533E',
-
-  black: '#1A1A1A',
-  black2: '#010101',
-
-  gray: '#3a3a3a',
-  gray2: '#AAAAAA',
-  gray3: '#CDCDCD',
-  gray4: '#B7B7B7',
-  gray5: '#F6F6F6',
-  gray6: '#F5F5F5',
-  gray7: '#666',
-  gray8: '#ECEBEB',
-
   white: '#FFFFFF',
-  white2: '#F7F7F7',
-  white3: '#EAEAEA',
-  white4: '#F8F8F8',
+  gray: {
+    '030': '#F9F9FB',
+    '050': '#F7F7FA',
+  },
 } as const;
