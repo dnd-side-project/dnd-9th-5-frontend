32a930bd803aee315833f6004dbe6a60c0682aa6
seondal <sseondal@gmail.com>
2023-08-04 19:25:15 +0900

    💄 style :  Header position fixed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import Tab from './Tab';
 
 export default function Header() {
   return (
-    <div className="border-b-divider border-b-2 px-20">
+    <div className="border-b-divider fixed inset-x-0 border-b-2 bg-white px-20 pt-16">
       <MainHeader />
       <Tab />
     </div>
