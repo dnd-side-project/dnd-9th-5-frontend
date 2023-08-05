27ea3f30b812208fa3c865e96ce5ad01274a1412
Kuesung Park <gueit214@naver.com>
2023-08-05 14:52:18 +0900

    feat : Portal 컴포넌트 구현

---
+++ src/components/Portal/Portal.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import useIsMounted from '@/hooks/useIsMounted';
+import { PropsWithChildren, useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+export type PortalProps = {
+  documentId?: string;
+};
+
+const findWrapperElement = (documentId: string): Element | null => {
+  const wrapper = document.getElementById(documentId);
+  if (wrapper) {
+    return wrapper;
+  } else {
+    console.warn(`Element with ID '${documentId}'가 root layout에 없어요....추가해주세요.`);
+    return null;
+  }
+};
+
+export default function Portal({ documentId, children }: PropsWithChildren<PortalProps>) {
+  const ref = useRef<Element | null>(null);
+  const isMounted = useIsMounted();
+
+  useEffect(() => {
+    if (documentId) {
+      const wrapper = findWrapperElement(documentId);
+      ref.current = wrapper;
+    } else {
+      ref.current = findWrapperElement('portal');
+    }
+  }, [isMounted, documentId]);
+
+  if (!(isMounted && ref.current)) return null;
+
+  return createPortal(children, ref.current);
+}
