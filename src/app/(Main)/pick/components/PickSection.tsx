b2a587e9ccfb1d13b59e39af84b0433e60917325
Kuesung Park <gueit214@naver.com>
2023-08-12 22:36:48 +0900

    feat : pick 페이지에 loading 구현

--- src/app/(Main)/pick/components/PickSection.tsx
+++ src/app/(Main)/pick/components/PickSection.tsx
@@ -4,11 +4,21 @@ import { BottomFixedButton } from '@/components/Button';
 import { Spacing } from '@/components/Spacing';
 import Image from 'next/image';
 import { useState } from 'react';
+import lottieJson from '../../../../../public/lotties/posepicker.json';
+import Lottie from 'react-lottie-player';
 
 const countList = ['1인', '2인', '3인', '4인', '5인+'];
 
 export default function PickSection() {
   const [countState, setCountState] = useState<string>('1인');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handlePickClick = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  };
 
   return (
     <section className="flex flex-col px-20">
@@ -22,11 +32,18 @@ export default function PickSection() {
           />
         ))}
       </div>
+
       <Spacing size={13} />
-      <div className="relative h-520 grow">
-        <Image src="/images/sample.png" fill priority alt="image" />
+      <div className="relative h-520">
+        {isLoading ? (
+          <Lottie loop animationData={lottieJson} play style={{ width: '100%', height: '100%' }} />
+        ) : (
+          <Image src="/images/sample.png" fill priority alt="image" />
+        )}
       </div>
-      <BottomFixedButton className="bg-main-violet text-white">포즈 pick!</BottomFixedButton>
+      <BottomFixedButton className="bg-main-violet text-white" onClick={handlePickClick}>
+        포즈 pick!
+      </BottomFixedButton>
     </section>
   );
 }
--- src/app/(Main)/talk/components/TalkSection.tsx
+++ src/app/(Main)/talk/components/TalkSection.tsx
@@ -1,15 +1,8 @@
 'use client';
 
 import Lottie from 'react-lottie-player';
-import lottieJson from '../../../../../public/jsons/pzpicker.json';
-import lottieJson2 from '../../../../../public/jsons/animation_ll796vsv.json';
 
 interface TalkSectionProps {}
 export default function TalkSection() {
-  return (
-    <section>
-      <Lottie loop animationData={lottieJson} play style={{ width: 150, height: 150 }} />
-      <Lottie loop animationData={lottieJson2} play style={{ width: 150, height: 150 }} />
-    </section>
-  );
+  return <section></section>;
 }
