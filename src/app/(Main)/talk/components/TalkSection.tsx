e11759e1f44bdf7d1d454c54d2b75533b0cc3d09
Kuesung Park <gueit214@naver.com>
2023-08-12 09:13:15 +0900

    feat : lottie 설치 및 파일 추가

--- src/app/(Main)/talk/components/TalkSection.tsx
+++ src/app/(Main)/talk/components/TalkSection.tsx
@@ -1,5 +1,15 @@
 'use client';
+
+import Lottie from 'react-lottie-player';
+import lottieJson from '../../../../../public/jsons/pzpicker.json';
+import lottieJson2 from '../../../../../public/jsons/animation_ll796vsv.json';
+
 interface TalkSectionProps {}
 export default function TalkSection() {
-  return <section></section>;
+  return (
+    <section>
+      <Lottie loop animationData={lottieJson} play style={{ width: 150, height: 150 }} />
+      <Lottie loop animationData={lottieJson2} play style={{ width: 150, height: 150 }} />
+    </section>
+  );
 }
