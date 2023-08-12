d015efd4635ca135d11efb9fdb98aecc13cd75a2
Kuesung Park <gueit214@naver.com>
2023-08-12 09:18:20 +0900

    refactor : 수정된 디자인에 맞게 수정

--- src/app/(Main)/pick/components/PickSection.tsx
+++ src/app/(Main)/pick/components/PickSection.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { BottomFixedButton } from '@/components/Button';
+import { Spacing } from '@/components/Spacing';
 import Image from 'next/image';
 import { useState } from 'react';
 
@@ -10,8 +11,8 @@ export default function PickSection() {
   const [countState, setCountState] = useState<string>('1인');
 
   return (
-    <section className="flex flex-col">
-      <div className="my-16 flex h-40 justify-evenly rounded-8 px-20">
+    <section className="flex flex-col px-20">
+      <div className="flex justify-evenly rounded-8 py-16">
         {countList.map((count) => (
           <CountItem
             key={count}
@@ -21,7 +22,8 @@ export default function PickSection() {
           />
         ))}
       </div>
-      <div className="relative h-520 grow bg-black">
+      <Spacing size={13} />
+      <div className="relative h-520 grow">
         <Image src="/images/sample.png" fill priority alt="image" />
       </div>
       <BottomFixedButton className="bg-main-violet text-white">포즈 pick!</BottomFixedButton>
@@ -38,7 +40,7 @@ interface CountItemProps {
 function CountItem({ isSelected, count, onClick }: CountItemProps) {
   return (
     <div
-      className={`flex grow cursor-pointer items-center justify-center first:rounded-l-8 last:rounded-r-8 ${
+      className={`flex h-40 grow cursor-pointer items-center justify-center first:rounded-l-8 last:rounded-r-8 ${
         isSelected
           ? 'border-1 border-main-violet bg-main-violet-bright'
           : 'border-1 border-default bg-sub-white'
