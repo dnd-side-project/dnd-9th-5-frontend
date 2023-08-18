2db4089d409050a130ce9548904a932ee9794d7c
seondal <sseondal@gmail.com>
2023-08-18 20:54:33 +0900

    ♻️ refactor :  EmptyCase 컴포넌트화

--- src/app/(Main)/feed/page.tsx
+++ src/app/(Main)/feed/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import EmptyCase from './components/EmptyCase';
 import Filter from './components/Filter';
 import FilterSheet from './components/FilterSheet';
 import PhotoList from './components/PhotoList';
@@ -9,6 +10,12 @@ export default function Feed() {
     <>
       <Filter />
       <div className="pt-72">
+        <EmptyCase
+          title={'신비한 포즈를 찾으시는군요!'}
+          text={'찾고 싶은 포즈를 저희에게 알려주세요.'}
+          button={'문의사항 남기기'}
+          path={''}
+        />
         <PhotoList />
       </div>
       <FilterSheet />
