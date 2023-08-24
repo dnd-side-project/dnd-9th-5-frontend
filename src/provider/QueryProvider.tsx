0805c9bc939dfe36f62b55b4d0c1d8e37bde99ac
seondal <sseondal@gmail.com>
2023-08-24 14:46:49 +0900

    🛠 fix :  QueryProvider defaultOption 제거

--- src/provider/QueryProvider.tsx
+++ src/provider/QueryProvider.tsx
@@ -8,15 +8,5 @@ import type { StrictPropsWithChildren } from '@/types';
 export default function QueryProvider({ children }: StrictPropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient());
 
-  // queryClient.setDefaultOptions({
-  //   queries: {
-  //     retry: 1,
-  //     refetchOnMount: false,
-  //     refetchOnWindowFocus: false,
-  //     refetchOnReconnect: false,
-  //     suspense: true,
-  //   },
-  // });
-
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
