b2f82fdfe71ba745a6adaa340d392f32c89fe287
Kuesung Park <gueit214@naver.com>
2023-08-25 20:42:10 +0900

    feat : Link로 라우팅

--- src/app/(Main)/components/MainHeader.tsx
+++ src/app/(Main)/components/MainHeader.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import Tab from './Tab';
 import { Header } from '@/components/Header';
@@ -6,7 +7,11 @@ import { Header } from '@/components/Header';
 export default function MainHeader() {
   return (
     <Header
-      leftNode={<h4>PosePicker</h4>}
+      leftNode={
+        <Link href="/pick">
+          <h4>PosePicker</h4>
+        </Link>
+      }
       rightNode={<Image src="/icons/menu.svg" width={24} height={24} alt="24" />}
       headerDownNode={<Tab />}
       className="px-20"
