d8f5743c6ac3792b5b4e3243942a5df05add036b
Kuesung Park <gueit214@naver.com>
2023-08-25 09:19:40 +0900

    refactor : Modal className확장

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,10 +5,12 @@ interface PopupProps {
   className?: string;
 }
 
-export default function Popup({ children }: StrictPropsWithChildren<PopupProps>) {
+export default function Popup({ className, children }: StrictPropsWithChildren<PopupProps>) {
   return (
     <ModalWrapper>
-      <section className="flex w-300 flex-col items-center rounded-16 bg-white px-16 py-12">
+      <section
+        className={`flex flex-col items-center rounded-16 bg-white px-16 py-12 ${className}`}
+      >
         {children}
       </section>
     </ModalWrapper>
