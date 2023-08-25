c539fca7361196ae96012c06a6d7dc833dcb4782
Kuesung Park <gueit214@naver.com>
2023-08-25 21:10:13 +0900

    feat : 미구현 기능 문구

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,14 +1,23 @@
 import ModalWrapper from './PortalWrapper';
 import { StrictPropsWithChildren } from '@/types';
 
-interface PopupProps {
+interface PopupProps extends React.HTMLAttributes<HTMLTableSectionElement> {
   className?: string;
+  onCloseOutside?: () => void;
 }
 
-export default function Popup({ className, children }: StrictPropsWithChildren<PopupProps>) {
+export default function Popup({
+  className,
+  children,
+  onCloseOutside,
+  ...props
+}: StrictPropsWithChildren<PopupProps>) {
   return (
-    <ModalWrapper>
-      <section className={`flex flex-col items-center  bg-white px-16 py-12 ${className}`}>
+    <ModalWrapper onClick={onCloseOutside}>
+      <section
+        className={`flex flex-col items-center  bg-white px-16 py-12 ${className}`}
+        {...props}
+      >
         {children}
       </section>
     </ModalWrapper>
