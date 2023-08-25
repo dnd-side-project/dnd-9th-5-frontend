bac4f3dec85951ffa2b21e0e47258bd7bfe026d0
Kuesung Park <gueit214@naver.com>
2023-08-25 09:19:47 +0900

    modal 너비 설정

--- src/app/(Sub)/detail/[id]/components/LinkShareModal.tsx
+++ src/app/(Sub)/detail/[id]/components/LinkShareModal.tsx
@@ -6,7 +6,7 @@ interface LinkShareModalProps {
 }
 export default function LinkShareModal({ onClose }: LinkShareModalProps) {
   return (
-    <Popup>
+    <Popup className="w-300">
       <p className="py-32">링크가 복사되었습니다.</p>
       <Button className="bg-main-violet text-white" onClick={onClose}>
         확인
