015be67cf03fedd9859a26ffa2d24d6c31fee010
Kuesung Park <gueit214@naver.com>
2023-08-26 11:20:36 +0900

    feat : 넓이, 높이 고정

--- src/app/(Main)/pick/components/PickSection.tsx
+++ src/app/(Main)/pick/components/PickSection.tsx
@@ -45,32 +45,34 @@ export default function PickSection() {
 
       <Spacing size={16} />
 
-      <div className="relative flex h-460 w-300 items-center justify-center">
-        {true && <Lottie loop animationData={lottiePick} play />}
-        <Image
-          src={image || '/images/image-frame.png'}
-          fill
-          alt="sample"
-          priority
-          loading="eager"
-          className={clsx({ hidden: isLoading }, 'cursor-pointer')}
-          onClick={() =>
-            open(({ exit }) => (
-              <Popup>
-                <Image
-                  src={image || '/images/image-frame.png'}
-                  alt="enlargementImage"
-                  priority
-                  loading="eager"
-                  onClick={exit}
-                  width={500}
-                  height={440}
-                  className="cursor-pointer"
-                />
-              </Popup>
-            ))
-          }
-        />
+      <div className="flex h-460 w-full items-center justify-center bg-black">
+        <div className="relative flex h-460 w-300 items-center justify-center">
+          {true && <Lottie loop animationData={lottiePick} play />}
+          <Image
+            src={image || '/images/image-frame.png'}
+            fill
+            alt="sample"
+            priority
+            loading="eager"
+            className={clsx({ hidden: isLoading }, 'cursor-pointer')}
+            onClick={() =>
+              open(({ exit }) => (
+                <Popup>
+                  <Image
+                    src={image || '/images/image-frame.png'}
+                    alt="enlargementImage"
+                    priority
+                    loading="eager"
+                    onClick={exit}
+                    width={500}
+                    height={440}
+                    className="cursor-pointer"
+                  />
+                </Popup>
+              ))
+            }
+          />
+        </div>
       </div>
 
       <BottomFixedButton
