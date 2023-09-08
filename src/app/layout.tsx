4d3a30e62e0d7c52a1ad001c5f493f1e54fff75c
seondal <sseondal@gmail.com>
2023-09-09 00:23:14 +0900

    📄 docs :  metatag 삽입

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,23 +6,30 @@ import { Suspense } from 'react';
 
 import { Analytics } from '@/components/Analytics';
 import { OverlayProvider } from '@/components/Overlay/OverlayProvider';
-import { BASE_SITE_URL, GA_ID } from '@/constants';
+import { BASE_SITE_URL } from '@/constants';
 import QueryProvider from '@/provider/QueryProvider';
 import RecoilContextProvider from '@/provider/RecoilContextProvider';
 
 import type { Metadata } from 'next';
+import Head from 'next/head';
 
 const DEFAULT_OG_TITLE = 'PosePicker';
-const DEFAULT_OG_DESC = '포토부스에서 고민하는 당신을 위한 포즈 추천';
+const DEFAULT_OG_DESC = '다음 포즈 뭐하지? 포즈피커가 포즈 고민을 해결해 드릴게요!';
 const DEFAULT_OG_IMAGE = '/images/main_star.png';
 
 export const metadata: Metadata = {
   metadataBase: new URL(BASE_SITE_URL),
   title: {
-    template: DEFAULT_OG_TITLE,
     default: DEFAULT_OG_TITLE,
+    template: `${DEFAULT_OG_TITLE} | %s`,
   },
   description: DEFAULT_OG_DESC,
+  verification: {
+    google: 'MB7qV_Oa4G4gR0jHgjtnE6S4g4blocE2mjo7z-z2f6Q',
+  },
+  other: {
+    'naver-site-verification': '65f3aba9349cce28018ac7a97d4f87ff00709aa3',
+  },
   openGraph: {
     title: DEFAULT_OG_TITLE,
     description: DEFAULT_OG_DESC,
@@ -49,6 +56,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <Head>
+        <meta name="naver-site-verification" content="65f3aba9349cce28018ac7a97d4f87ff00709aa3" />
+        <meta
+          name="google-site-verification"
+          content="MB7qV_Oa4G4gR0jHgjtnE6S4g4blocE2mjo7z-z2f6Q"
+        />
+      </Head>
       <body className="flex min-h-[100dvh] w-screen touch-none justify-center bg-slate-100 py-px">
         <div className="w-full max-w-440 overflow-hidden bg-white text-primary">
           <Suspense>
