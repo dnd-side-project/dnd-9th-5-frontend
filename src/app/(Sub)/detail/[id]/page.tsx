d86dc7283a3c150abb538a953c3cdad749cbe60b
seondal <sseondal@gmail.com>
2023-09-10 04:53:34 +0900

    🌐 seo :  디테일 페이지 동적으로 메타데이터 생성

--- src/app/(Sub)/detail/[id]/page.tsx
+++ src/app/(Sub)/detail/[id]/page.tsx
@@ -1,7 +1,19 @@
+import { Metadata, ResolvingMetadata } from 'next';
 import DetailHeader from './components/DetailHeader';
 import DetailSection from './components/DetailSection';
 import { getPoseDetail } from '@/apis';
 import { HydrationProvider } from '@/components/Provider/HydrationProvider';
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const id = parseInt(params.id);
+  const {
+    poseInfo: { peopleCount, frameCount, tagAttributes },
+  } = await getPoseDetail(id);
+  const description = `${tagAttributes},${frameCount}컷,${peopleCount}인 포즈추천`;
+  return {
+    description,
+  };
+}
 
 export default function DetailPage({ params }: { params: { id: number } }) {
   const { id } = params;
