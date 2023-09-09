db47abe1c34108096bf8c5d5bd48f1914555d15a
Kuesung Park <gueit214@naver.com>
2023-09-09 12:16:39 +0900

    refactor : Feed 페이지 Hydration 제거

--- src/app/(Main)/feed/page.tsx
+++ src/app/(Main)/feed/page.tsx
@@ -2,11 +2,9 @@ import { QueryAsyncBoundary } from '@suspensive/react-query';
 import { Metadata } from 'next';
 
 import FeedSection from './FeedSection';
-import { getPoseFeed } from '@/apis';
 import { RejectedFallback } from '@/components/ErrorBoundary';
 import { Loading } from '@/components/Loading';
 import { PageAnimation } from '@/components/PageAnimation';
-import { HydrationProvider } from '@/components/Provider/HydrationProvider';
 
 export const metadata: Metadata = {
   title: '포즈피드',
@@ -19,13 +17,7 @@ export default function Feed() {
       pendingFallback={<Loading className="h-[calc(100dvh-178px)]" />}
     >
       <PageAnimation>
-        <HydrationProvider
-          queryKey={['poseFeed']}
-          queryFn={() => getPoseFeed(0, 0, '', 0)}
-          isInfiniteQuery
-        >
-          <FeedSection />
-        </HydrationProvider>
+        <FeedSection />
       </PageAnimation>
     </QueryAsyncBoundary>
   );
