0b1c57cc158df3949d3373f0b16d2db29cc32571
seondal <sseondal@gmail.com>
2023-09-10 06:38:39 +0900

    🛠 fix :  이미지 모달 비율에 맞게 채우기

--- src/components/Modal/ImageModal.client.tsx
+++ src/components/Modal/ImageModal.client.tsx
@@ -1,9 +1,6 @@
-'use client';
 import Image from 'next/image';
-import { useEffect } from 'react';
 
 import ModalWrapper from './ModalWrapper';
-import { Spacing } from '../Spacing';
 
 interface ImageModalProps {
   image: string;
@@ -11,24 +8,11 @@ interface ImageModalProps {
 }
 
 export default function ImageModal({ image, onClose }: ImageModalProps) {
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    return () => {
-      document.body.style.overflow = 'unset';
-    };
-  }, []);
-
   return (
     <ModalWrapper onClose={onClose} className="w-full">
-      <Spacing size={12} />
-      <Image
-        src={image}
-        alt="fullImage"
-        className="object-cover"
-        width={500}
-        height={500}
-        onClick={onClose}
-      />
+      <div className="h-screen w-screen">
+        <Image src={image} alt="fullImage" fill className="object-contain" onClick={onClose} />
+      </div>
     </ModalWrapper>
   );
 }
