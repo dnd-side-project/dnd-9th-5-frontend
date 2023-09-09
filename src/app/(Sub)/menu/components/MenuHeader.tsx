2299a4b33d559457eefdff421fe38117f2aef870
Kuesung Park <gueit214@naver.com>
2023-09-09 10:00:51 +0900

    refactor : 뒤로가기로 수정

--- src/app/(Sub)/menu/components/MenuHeader.tsx
+++ src/app/(Sub)/menu/components/MenuHeader.tsx
@@ -1,11 +1,18 @@
+'use client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import IconButton from '@/components/Button/IconButton';
 import { Header } from '@/components/Header';
 import { Spacing } from '@/components/Spacing';
-import CloseButton from '@/components/Header/CloseButton';
 
 export default function MenuHeader() {
+  const router = useRouter();
   const LeftNode = (
     <div className="flex items-center px-4">
-      <CloseButton />
+      <IconButton size="large" onClick={() => router.back()}>
+        <Image src="/icons/close.svg" width={24} height={24} alt="back" />
+      </IconButton>
       <Spacing size={12} direction="horizontal" />
       <h4>메뉴</h4>
     </div>
