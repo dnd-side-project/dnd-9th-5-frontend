1694374dd639910266d78305bcdf8a9064f60a0f
seondal <sseondal@gmail.com>
2023-09-10 05:20:17 +0900

    🌐 seo :  사이트맵 동적으로 생성

--- src/app/(Sub)/detail/[id]/page.tsx
+++ src/app/(Sub)/detail/[id]/page.tsx
@@ -4,15 +4,21 @@ import DetailSection from './components/DetailSection';
 import { getPoseDetail } from '@/apis';
 import { HydrationProvider } from '@/components/Provider/HydrationProvider';
 
-export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+export async function generateMetadata(
+  { params }: { params: { id: string } },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
   const id = parseInt(params.id);
   const {
     poseInfo: { peopleCount, frameCount, tagAttributes },
   } = await getPoseDetail(id);
   const description = `${tagAttributes},${frameCount}컷,${peopleCount}인 포즈추천`;
+  const defaultOgTitle = (await parent).openGraph?.title;
+
   return {
     description,
     openGraph: {
+      title: defaultOgTitle,
       description: '이 포즈는 어때요?',
     },
   };
