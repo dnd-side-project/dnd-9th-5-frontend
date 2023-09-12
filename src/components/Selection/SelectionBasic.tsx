9aac37990ed66721c2d5cada5b50394874e879c2
Kuesung Park <gueit214@naver.com>
2023-09-12 10:35:10 +0900

    refactor : 불필요한 depth 제거

--- src/components/Selection/SelectionBasic.tsx
+++ src/components/Selection/SelectionBasic.tsx
@@ -9,25 +9,21 @@ export default function SelectionBasic({ data, state, setState, onChangeState }:
   return (
     <div className="flex justify-evenly rounded-8">
       {data.map((item) => (
-        <div
+        <span
           key={item.text}
+          id="subtitle-2"
           className={`flex h-40 grow cursor-pointer items-center justify-center first:rounded-l-8 last:rounded-r-8 ${
             state === item.value
-              ? 'border-1 border-main-violet bg-main-violet-bright'
-              : 'border-1 border-border-default bg-sub-white'
+              ? 'border-1 border-main-violet bg-main-violet-bright text-main-violet-dark'
+              : 'border-1 border-border-default bg-sub-white text-secondary'
           }`}
           onClick={() => {
             setState(item.value);
             onChangeState && onChangeState();
           }}
         >
-          <div
-            id="subtitle-2"
-            className={state === item.value ? 'text-main-violet-dark' : 'text-secondary'}
-          >
-            {item.text}
-          </div>
-        </div>
+          {item.text}
+        </span>
       ))}
     </div>
   );
