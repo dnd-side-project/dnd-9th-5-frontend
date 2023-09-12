7481e2b21989c5951997358a515ef401aa325676
Kuesung Park <gueit214@naver.com>
2023-09-12 10:26:21 +0900

    fix : 타입 에러

--- src/components/Selection/SelectionBasic.tsx
+++ src/components/Selection/SelectionBasic.tsx
@@ -2,9 +2,10 @@ interface SelectionBasic {
   data: Array<{ text: string; value: number }>;
   state: number;
   setState: React.Dispatch<React.SetStateAction<number>>;
+  onChangeState?: () => void;
 }
 
-export default function SelectionBasic({ data, state, setState }: SelectionBasic) {
+export default function SelectionBasic({ data, state, setState, onChangeState }: SelectionBasic) {
   return (
     <div className="flex justify-evenly rounded-8">
       {data.map((item) => (
@@ -15,7 +16,10 @@ export default function SelectionBasic({ data, state, setState }: SelectionBasic
               ? 'border-1 border-main-violet bg-main-violet-bright'
               : 'border-1 border-border-default bg-sub-white'
           }`}
-          onClick={() => setState(item.value)}
+          onClick={() => {
+            setState(item.value);
+            onChangeState && onChangeState();
+          }}
         >
           <div
             id="subtitle-2"
