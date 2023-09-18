ed8b6374e0ee2d0c022d56e888b9e8d9f00aec7c
seondal <sseondal@gmail.com>
2023-09-18 18:25:46 +0900

    ✨ feat :  메뉴화면 서비스 정보 추가

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -2,4 +2,5 @@ export const URL = {
   site: 'https://www.posepicker.site',
   inquiry:
     'https://docs.google.com/forms/d/e/1FAIpQLSeZuPZTXnO4rZ4k39SzXv96PWAW4gLcTYBrsRUrgRHSVV9Ldg/viewform?usp=sf_link',
+  information: 'https://shineshine.notion.site/a668d9eba61f48e584df2ad3a946c313',
 };
