1effba4f22839581239676dc48ac2c9b7cd0127d
seondal <sseondal@gmail.com>
2023-09-18 17:04:34 +0900

    ✨ feat :  메타태그 페이지별로 다르게 한거 취소

--- src/app/(Main)/feed/page.tsx
+++ src/app/(Main)/feed/page.tsx
@@ -1,16 +1,9 @@
 import { QueryAsyncBoundary } from '@suspensive/react-query';
-import { Metadata } from 'next';
 
 import FeedSection from './FeedSection';
 import { RejectedFallback } from '@/components/ErrorBoundary';
 import { Loading } from '@/components/Loading';
 import { PageAnimation } from '@/components/PageAnimation';
-import { META_STRING } from '@/constants/meta';
-
-export const metadata: Metadata = {
-  title: '포즈피드',
-  description: META_STRING.description.feed,
-};
 
 export default function Feed() {
   return (
--- src/app/(Main)/talk/page.tsx
+++ src/app/(Main)/talk/page.tsx
@@ -1,15 +1,7 @@
-import { Metadata } from 'next';
-
 import TalkSection from './components/TalkSection';
 import TitleSection from './components/TitleSection';
 import { PageAnimation } from '@/components/PageAnimation';
 import { Spacing } from '@/components/Spacing';
-import { META_STRING } from '@/constants/meta';
-
-export const metadata: Metadata = {
-  title: '포즈톡',
-  description: META_STRING.description.talk,
-};
 
 export default function Talk() {
   return (
