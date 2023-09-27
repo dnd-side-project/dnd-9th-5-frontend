6efc12ee91e50871de4dc0348a0e709ab4133b3d
Kuesung Park <gueit214@naver.com>
2023-09-27 21:42:04 +0900

    feat : 카카오 로그인

--- src/app/(Sub)/menu/components/LoginSection.tsx
+++ src/app/(Sub)/menu/components/LoginSection.tsx
@@ -1,9 +1,10 @@
 'use client';
 
+import Script from 'next/script';
+
 import { Icon } from '@/components/Icon';
-import { PreparingModal } from '@/components/Modal';
-import { useOverlay } from '@/components/Overlay/useOverlay';
 import { Spacing } from '@/components/Spacing';
+import { kakaoInit, kakaoLogin } from '@/utils/kakaoLogin';
 
 function DefaultProfile() {
   return (
@@ -14,17 +15,15 @@ function DefaultProfile() {
 }
 
 export default function LoginSection() {
-  const { open } = useOverlay();
-
   return (
     <section className="py-12">
-      <div
-        className="bg-violet flex items-center rounded-16 bg-main-violet-bright px-20 py-24"
-        onClick={() => open(({ exit }) => <PreparingModal onClose={exit} />)}
-      >
+      <Script src="https://developers.kakao.com/sdk/js/kakao.js" onLoad={kakaoInit} />
+      <div className="bg-violet flex items-center rounded-16 bg-main-violet-bright px-20 py-24">
         <DefaultProfile />
         <Spacing size={16} direction="horizontal" />
-        <div id="subtitle-1">로그인하기</div>
+        <div id="subtitle-1" onClick={kakaoLogin}>
+          로그인하기
+        </div>
       </div>
     </section>
   );
