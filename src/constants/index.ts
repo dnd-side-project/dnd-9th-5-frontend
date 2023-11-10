93175119e68968904e8ae26a8503947753d2e481
seondal <sseondal@gmail.com>
2023-11-10 16:26:02 +0900

    🛠 fix :  site copy //

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const SITE_URL = 'https://www.posepicker.site/';
+export const SITE_URL = 'https://www.posepicker.site';
 export const BASE_API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const BASE_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 export const KAKAO_KEY = process.env.NEXT_PUBLIC_KAKAO_KEY;
