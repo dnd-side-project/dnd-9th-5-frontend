3bcfc538bf4cf49d1571f94df063f07fcedc3b8f
seondal <sseondal@gmail.com>
2024-01-10 02:30:01 +0900

    🛠 fix :  일단 로그인 막아놓기

--- src/app/(Sub)/menu/components/LoginSection.tsx
+++ src/app/(Sub)/menu/components/LoginSection.tsx
@@ -4,6 +4,7 @@ import { useRecoilValue } from 'recoil';
 
 import LoginModal from './LoginModal';
 import { Icon } from '@/components/Button/Icon';
+import { PreparingModal } from '@/components/Modal';
 import { useOverlay } from '@/components/Overlay/useOverlay';
 import { Spacing } from '@/components/Spacing';
 import { ICON } from '@/constants/icon';
@@ -21,7 +22,8 @@ export default function LoginSection() {
   const { open, exit } = useOverlay();
 
   const isLogin = useRecoilValue(isLoginAtom);
-  const userData = useRecoilValue(userAtom);
+  // const userData = useRecoilValue(userAtom);
+  // console.log('🚀 ~ LoginSection ~ userData:', userData);
 
   return (
     <section className="py-12">
@@ -29,12 +31,12 @@ export default function LoginSection() {
         <div className="bg-violet flex w-full items-center rounded-16 bg-main-violet-base px-20 py-24">
           <DefaultProfile />
           <Spacing size={16} direction="horizontal" />
-          <span id="subtitle-1">{userData.email}</span>
+          <span id="subtitle-1">(개발중) 로그인 완료</span>
         </div>
       ) : (
         <button
           className="bg-violet flex w-full items-center rounded-16 bg-main-violet-base px-20 py-24"
-          onClick={() => open(() => <LoginModal onClose={exit} />)}
+          onClick={() => open(() => <PreparingModal onClose={exit} />)}
         >
           <DefaultProfile />
           <Spacing size={16} direction="horizontal" />
