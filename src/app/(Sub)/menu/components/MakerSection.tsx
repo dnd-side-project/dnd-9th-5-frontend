fc0654cd902926cb69cb65471e5fc250b9f9a170
seondal <sseondal@gmail.com>
2024-01-10 00:05:36 +0900

    📦 add :  메뉴 하단 아이콘 변경

--- src/app/(Sub)/menu/components/MakerSection.tsx
+++ src/app/(Sub)/menu/components/MakerSection.tsx
@@ -1,28 +1,29 @@
 'use client';
 
-import { IconButton } from '@/components/Button';
+import { Icon } from '@/components/Button';
 import { BottomFixedDiv } from '@/components/Button/BottomFixedDiv';
 import { Spacing } from '@/components/Spacing';
 
 export default function MakerSection() {
   return (
     <BottomFixedDiv>
-      <div className="flex justify-center">
-        <IconButton
-          icon="instagram"
-          onClick={() => window.open('https://www.instagram.com/posepicker')}
-        />
-        <Spacing size={16} direction="horizontal" />
-        <IconButton
-          icon="github"
-          onClick={() => window.open('https://github.com/dnd-side-project/dnd-9th-5-frontend')}
-        />
+      <div>
+        <div className="flex justify-center gap-16">
+          <button onClick={() => window.open('https://www.instagram.com/posepicker')}>
+            <Icon icon="instagram" size={48} />
+          </button>
+          <button
+            onClick={() => window.open('https://github.com/dnd-side-project/dnd-9th-5-frontend')}
+          >
+            <Icon icon="github" size={48} />
+          </button>
+        </div>
+        <Spacing size={8} />
+        <div className="flex justify-center">
+          <p className="text-12 text-caption">© POSEPICKER</p>
+        </div>
+        <Spacing size={15} />
       </div>
-      <Spacing size={8} />
-      <div className="flex justify-center">
-        <p className="text-12 text-caption">© POSEPICKER</p>
-      </div>
-      <Spacing size={60} />
     </BottomFixedDiv>
   );
 }
