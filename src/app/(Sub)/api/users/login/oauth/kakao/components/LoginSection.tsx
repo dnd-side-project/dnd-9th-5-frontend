360dffb732e60198dc75602bdfd505c908748b77
seondal <sseondal@gmail.com>
2024-02-19 19:09:58 +0900

    🛠 fix :  기존 삭제 22

--- src/app/(Sub)/api/users/login/oauth/kakao/components/LoginSection.tsx
+++ src/app/(Sub)/api/users/login/oauth/kakao/components/LoginSection.tsx
@@ -1,31 +1,31 @@
-'use client';
+// 'use client';
 
-import { useRouter } from 'next/navigation';
-import { useSetRecoilState } from 'recoil';
+// import { useRouter } from 'next/navigation';
+// import { useSetRecoilState } from 'recoil';
 
-import { useRegisterQuery } from '@/apis';
-import { Loading } from '@/components/Loading';
-import { isLoginAtom, userAtom } from '@/context/userState';
-import { setCookie } from '@/utils/cookieController';
+// import { useRegisterQuery } from '@/apis';
+// import { Loading } from '@/components/Loading';
+// import { isLoginAtom, userAtom } from '@/context/userState';
+// import { setCookie } from '@/utils/cookieController';
 
-interface LoginSectionProps {
-  code: string;
-}
-export default function LoginSection({ code }: LoginSectionProps) {
-  const router = useRouter();
-  const { data } = useRegisterQuery(code);
-  console.log(data);
+// interface LoginSectionProps {
+//   code: string;
+// }
+// export default function LoginSection({ code }: LoginSectionProps) {
+//   const router = useRouter();
+//   const { data } = useRegisterQuery(code);
+//   console.log(data);
 
-  const setLoginState = useSetRecoilState(isLoginAtom);
-  const setUserState = useSetRecoilState(userAtom);
-  setLoginState(true);
-  setUserState(data);
+//   const setLoginState = useSetRecoilState(isLoginAtom);
+//   const setUserState = useSetRecoilState(userAtom);
+//   setLoginState(true);
+//   setUserState(data);
 
-  const { token } = data;
-  const { accessToken, refreshToken } = token;
-  setCookie('accessToken', accessToken);
-  setCookie('refreshToken', refreshToken);
-  router.replace('/menu');
+//   const { token } = data;
+//   const { accessToken, refreshToken } = token;
+//   setCookie('accessToken', accessToken);
+//   setCookie('refreshToken', refreshToken);
+//   router.replace('/menu');
 
-  return <Loading />;
-}
+//   return <Loading />;
+// }
