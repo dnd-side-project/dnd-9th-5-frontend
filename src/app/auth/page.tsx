f4f04056f6ad8161588ac83ec41fd132072706d3
seondal <sseondal@gmail.com>
2024-02-21 00:23:26 +0900

    🏗 folder :  redirect uri 드디어 간단하게 변경!

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,23 +2,19 @@
 
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
-// import { useSetRecoilState } from 'recoil';
 
 import { getRegister } from '@/apis';
-// import { userAtom } from '@/context/userState';
 
 export default function Page() {
   const code = useSearchParams().get('code');
-  // const router = useRouter();
-  // const setUserState = useSetRecoilState(userAtom);
+  const router = useRouter();
 
   useEffect(() => {
     if (code) {
       console.log(code);
       getRegister(code).then((res) => {
         console.log(res);
-        // setUserState(res);
-        // router.replace('/menu');
+        router.replace('/menu');
       });
     }
   });
