c59d06b05997ee869375a40d5d0eea5012c91952
seondal <sseondal@gmail.com>
2024-03-22 00:43:15 +0900

    💄 style :  서비스 탈퇴 페이지

--- src/app/(Sub)/menu/withdraw/page.tsx
+++ src/app/(Sub)/menu/withdraw/page.tsx
@@ -9,18 +9,22 @@ import { useOverlay } from '@/components/Overlay/useOverlay';
 import { withdrawReasonList } from '@/constants/data';
 
 export default function Page() {
-  const [isButtonActive, setIsButtonActive] = useState(false);
-  const [isEtcSelected, setIsEtcSelected] = useState(false);
-  const [etc, setEtc] = useState<string>('');
+  const [withdrawalReason, setWithdrawalReason] = useState<string>();
+  const [etc, setEtc] = useState(false);
   const { open } = useOverlay();
 
-  function handleSelectChange(e: any) {
-    setIsButtonActive(true);
-    setIsEtcSelected(e.target?.id === 'etc');
-  }
-
-  function handleEtcTextarea(e: any) {
-    setEtc(e.target.value);
+  function handleWithdraw() {
+    open(({ exit }) => (
+      <Popup
+        title="정말 탈퇴하시겠어요?"
+        content={`탈퇴 시 업로드한 포즈를 제외한\n모든 정보가 삭제되며 복구되지 않아요.`}
+      >
+        <>
+          <PrimaryButton onClick={exit} text="취소" />
+          <PrimaryButton text="탈퇴" />
+        </>
+      </Popup>
+    ));
   }
 
   return (
@@ -32,51 +36,47 @@ export default function Page() {
           <br />
           알려줄 수 있나요?
         </h4>
-        <form onChange={(e) => handleSelectChange(e)}>
+        <form>
           {withdrawReasonList.map((item, idx) => (
-            <div key={idx} className="py-16">
-              <input type="radio" value={item} name="withdrawalReason" />
+            <div
+              key={idx}
+              className="py-16"
+              onClick={() => {
+                setWithdrawalReason(item);
+                setEtc(false);
+              }}
+            >
+              {item === withdrawalReason && <span>wow</span>}
               <label className="ml-8">{item}</label>
             </div>
           ))}
-          <div className="py-16">
-            <input type="radio" value={etc} name="withdrawalReason" id="etc" />
+          <div
+            className="py-16"
+            onClick={() => {
+              setEtc(true);
+              setWithdrawalReason(undefined);
+            }}
+          >
             <label className="ml-8">기타</label>
           </div>
-          <BottomFixedDiv>
-            <PrimaryButton text="계속 쓸래요" type="outline" />
-            {isButtonActive ? (
-              <PrimaryButton
-                text="탈퇴할래요"
-                type="fill"
-                onClick={() =>
-                  open(({ exit }) => (
-                    <Popup
-                      title="정말 탈퇴하시겠어요?"
-                      content={`탈퇴 시 업로드한 포즈를 제외한\n모든 정보가 삭제되며 복구되지 않아요.`}
-                    >
-                      <>
-                        <PrimaryButton onClick={exit} text="취소" />
-                        <PrimaryButton buttonType="submit" text="탈퇴" />
-                      </>
-                    </Popup>
-                  ))
-                }
-              />
-            ) : (
-              <PrimaryButton text="탈퇴할래요" type="secondary" />
-            )}
-          </BottomFixedDiv>
         </form>
-        {isEtcSelected && (
+        {etc && (
           <textarea
             className="border-border-default outline-border-active"
-            onChange={(e) => handleEtcTextarea(e)}
-            value={etc}
+            onChange={(e) => setWithdrawalReason(e.target.value)}
+            value={withdrawalReason}
             placeholder="떠나시는 이유를 알려주세요"
           />
         )}
       </div>
+      <BottomFixedDiv>
+        <PrimaryButton text="계속 쓸래요" type="outline" />
+        {withdrawalReason ? (
+          <PrimaryButton text="탈퇴할래요" type="fill" onClick={handleWithdraw} />
+        ) : (
+          <PrimaryButton text="탈퇴할래요" type="secondary" />
+        )}
+      </BottomFixedDiv>
     </>
   );
 }
