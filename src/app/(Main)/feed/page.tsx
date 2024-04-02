4901d5de7deb2ec2b83af02ba1bd676f2733553b
seondal <sseondal@gmail.com>
2024-04-03 00:24:36 +0900

    🏗 folder :  FeedSection 컴포넌트화

--- src/app/(Main)/feed/page.tsx
+++ src/app/(Main)/feed/page.tsx
@@ -1,16 +1,20 @@
 'use client';
 
 import { QueryAsyncBoundary } from '@suspensive/react-query';
+import Link from 'next/link';
 
 import FilterSheet from './components/FilterSheet';
 import FilterTab from './components/FilterTab';
-import FeedSection from './FeedSection';
 import { usePoseFeedQuery } from '@/apis';
+import { PrimaryButton } from '@/components/Button';
 import { RejectedFallback } from '@/components/ErrorBoundary';
+import EmptyCase from '@/components/Feed/EmptyCase';
+import FeedSection from '@/components/Feed/FeedSection';
 import { MainHeader } from '@/components/Header';
 import { Loading } from '@/components/Loading';
 import { PageAnimation } from '@/components/PageAnimation';
 import { Spacing } from '@/components/Spacing';
+import { URL } from '@/constants/url';
 import useFilterState from '@/hooks/useFilterState';
 
 export default function Feed() {
@@ -27,7 +31,16 @@ export default function Feed() {
           <FilterTab />
         </MainHeader>
         <Spacing size={50} />
-        <FeedSection data={data} fetchNextPage={fetchNextPage} />
+        <FeedSection data={data} fetchNextPage={fetchNextPage}>
+          <EmptyCase
+            title={'신비한 포즈를 찾으시는군요!'}
+            text={'찾고 싶은 포즈를 저희에게 알려주세요.'}
+          >
+            <Link href={URL.inquiry}>
+              <PrimaryButton text={'문의사항 남기기'} />
+            </Link>
+          </EmptyCase>
+        </FeedSection>
         <FilterSheet />
       </PageAnimation>
     </QueryAsyncBoundary>
--- src/components/Feed/FeedSection.tsx
+++ src/components/Feed/FeedSection.tsx
@@ -5,23 +5,20 @@ import {
   InfiniteData,
   InfiniteQueryObserverResult,
 } from '@tanstack/react-query';
-import Link from 'next/link';
-import { useEffect } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 import { PoseFeedResponse } from '@/apis';
-import { PrimaryButton } from '@/components/Button';
-import EmptyCase from '@/components/Feed/EmptyCase';
 import PhotoList from '@/components/Feed/PhotoList';
-import { URL } from '@/constants/url';
 
-interface FeedSecionI {
+interface FeedSecionI extends PropsWithChildren {
   data: InfiniteData<PoseFeedResponse>;
   fetchNextPage: (
     options?: FetchNextPageOptions | undefined
   ) => Promise<InfiniteQueryObserverResult<PoseFeedResponse, unknown>>;
 }
-export default function FeedSection({ data, fetchNextPage }: FeedSecionI) {
+
+export default function FeedSection({ data, fetchNextPage, children }: FeedSecionI) {
   const { ref, inView } = useInView();
 
   useEffect(() => {
@@ -31,14 +28,7 @@ export default function FeedSection({ data, fetchNextPage }: FeedSecionI) {
   return (
     <div>
       {data.pages[0].filteredContents.empty ? (
-        <EmptyCase
-          title={'신비한 포즈를 찾으시는군요!'}
-          text={'찾고 싶은 포즈를 저희에게 알려주세요.'}
-        >
-          <Link href={URL.inquiry}>
-            <PrimaryButton text={'문의사항 남기기'} />
-          </Link>
-        </EmptyCase>
+        children
       ) : (
         <div className="columns-2	py-16">
           {data.pages.map((page) => (
