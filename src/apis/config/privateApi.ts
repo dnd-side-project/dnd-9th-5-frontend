2f578a4632641a0b26461723e5219111b6a376e4
seondal <sseondal@gmail.com>
2024-04-12 23:22:20 +0900

    ✨ feat :  axios interceptor request 오류 발생 시 자동 로그아웃, 회원탈퇴 일단 보이게

--- src/apis/config/privateApi.ts
+++ src/apis/config/privateApi.ts
@@ -27,14 +27,25 @@ privateApi.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-privateApi.interceptors.request.use((config) => {
-  const accessToken = getAccesstoken();
 
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
+privateApi.interceptors.request.use(
+  (config) => {
+    const accessToken = getAccesstoken();
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    if (error.response.status === 415) {
+      alert('세션이 만료되었어요. 다시 로그인이 필요해요!');
+    } else {
+      alert('오류가 발생했어요.');
+    }
+    location.href = '/auth/logout';
   }
-
-  return config;
-});
+);
 
 export default privateApi;
--- src/app/auth/withdraw/page.tsx
+++ src/app/auth/withdraw/page.tsx
@@ -3,7 +3,7 @@
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
-import { patchDeleteAccount } from '@/apis';
+import { patchDeleteAccount, patchLogout } from '@/apis';
 import useUserState from '@/context/userState';
 
 export default function Page() {
@@ -12,15 +12,23 @@ export default function Page() {
   const withdrawalReason = useSearchParams().get('reason');
 
   useEffect(() => {
-    if (token && withdrawalReason) {
-      patchDeleteAccount(token.accessToken, token.refreshToken, withdrawalReason).then(
-        (response) => {
-          console.log(response);
-          clearUser();
-        }
-      );
+    // if (token && withdrawalReason) {
+    //   patchDeleteAccount(token.accessToken, token.refreshToken, withdrawalReason).then(
+    //     (response) => {
+    //       console.log(response);
+    //       clearUser();
+    //     }
+    //   );
+    // }
+    // 회원탈퇴 api 될 떄 까지만
+    if (token) {
+      patchLogout(token.accessToken, token.refreshToken).then(() => {
+        alert('로그아웃 되었습니다');
+      });
     }
-    router.back();
+    clearUser();
+    localStorage.removeItem('accesstoken');
+    router.replace('/menu');
   });
 
   return;
