b53b9b20a88349b864ed0e0b64ba41c4d644a5e2
seondal <sseondal@gmail.com>
2024-05-07 23:34:25 +0900

    💄 style :  앱 설치 안내 배너 위쪽

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -22,10 +22,10 @@ export default function Header({
       <div id="ios-banner">
         <Spacing size={62} />
       </div>
-      <div id="ios-banner">
-        <Banner />
-      </div>
       <div className="fixed inset-x-0 top-0 z-30 mx-auto max-w-440 bg-white">
+        <div id="ios-banner">
+          <Banner />
+        </div>
         <div className="flex h-48 items-center justify-between gap-12 px-4 pt-8">
           {close ? <CloseButton /> : <div className="w-4" />}
           <h4 className="flex flex-1">{title}</h4>
