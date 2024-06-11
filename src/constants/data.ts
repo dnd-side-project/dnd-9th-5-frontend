f82ae9a10ece3dd9e3e53ca34cf6397bc8ca5438
seondal <sseondal@gmail.com>
2024-06-11 23:28:48 +0900

    ✨ feat :  릴리즈노트 메뉴에 추가

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -10,6 +10,8 @@ export const menuList: readonly MenuI[] = [
   { text: '자주 묻는 질문', link: URL.menu.faq },
   { text: '포즈피커 공식 SNS', link: URL.instagram, highlight: true },
   { text: '문의하기', link: URL.inquiry },
+  { text: '문의하기', link: URL.inquiry },
+  { text: '개발자의 비밀일기', link: URL.menu.release_note },
   { text: '', link: '' },
   { text: '이용약관', link: URL.menu.term },
   { text: '개인정보 처리방침', link: URL.menu.privacy },
