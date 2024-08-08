8030cfb2e9018dae7bb88a01e9f440add7b32358
guesung <gueit214@naver.com>
2024-08-09 01:24:51 +0900

    ♻️ refactor: then-catch문을 async-await문으로 수정하여 가독성 향상

--- src/app/auth/withdraw/components/WithdrawComponent.tsx
+++ src/app/auth/withdraw/components/WithdrawComponent.tsx
@@ -1,24 +1,26 @@
 'use client';
 
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
 
 import { patchDeleteAccount } from '@/apis';
 import useUserState from '@/context/userState';
+import useDidMount from '@/hooks/useDidMount';
 
 export default function WithdrawComponent() {
   const router = useRouter();
   const { token, clearUser } = useUserState();
   const withdrawalReason = useSearchParams().get('reason');
 
-  useEffect(() => {
+  useDidMount(async () => {
     if (token && withdrawalReason) {
-      patchDeleteAccount(token.accessToken, token.refreshToken, withdrawalReason).then(
-        (response) => {
-          console.log(response);
-        }
+      const response = await patchDeleteAccount(
+        token.accessToken,
+        token.refreshToken,
+        withdrawalReason
       );
+      console.log(response);
     }
+
     // 회원탈퇴 api 될 떄 까지만
     // if (token) {
     //   patchLogout(token.accessToken, token.refreshToken).then(() => {
