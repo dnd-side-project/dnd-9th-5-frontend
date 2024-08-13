905ec6bbd2c057fe2c22e20a2f5804a2d9b40ae6
guesung <gueit214@naver.com>
2024-08-13 12:46:45 +0900

    refactor: feed페이지 컴포넌트를 분리한다

--- src/app/(Main)/feed/page.tsx
+++ src/app/(Main)/feed/page.tsx
@@ -1,23 +1,5 @@
-'use client';
-
-import { QueryAsyncBoundary } from '@suspensive/react-query';
-
-import FeedContent from './FeedContent';
-import FilterSheet from './FilterSheet';
-import { RejectedFallback } from '@/components/ErrorBoundary';
-import { Loading } from '@/components/Loading';
-import { PageAnimation } from '@/components/PageAnimation';
+import FeedComponent from './FeedComponent';
 
 export default function Feed() {
-  return (
-    <QueryAsyncBoundary
-      rejectedFallback={RejectedFallback}
-      pendingFallback={<Loading className="h-[calc(100dvh-178px)]" />}
-    >
-      <PageAnimation>
-        <FeedContent />
-        <FilterSheet />
-      </PageAnimation>
-    </QueryAsyncBoundary>
-  );
+  return <FeedComponent />;
 }
