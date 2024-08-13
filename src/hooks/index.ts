128221240c520a483467649725a397c7dce1e861
guesung <gueit214@naver.com>
2024-08-13 13:17:24 +0900

    refactor: 사용하지 않는 훅을 제거한다

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,5 +4,3 @@ export { default as useFilterState } from './useFilterState';
 export { default as useIsMounted } from './useIsMounted';
 export { default as useKakaoShare } from './useKakaoShare';
 export { default as useLaunchApp } from './useLaunchApp';
-export { default as useLoading } from './useLoading';
-export { default as useObserver } from './useObserver';
