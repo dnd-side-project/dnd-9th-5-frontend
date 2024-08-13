619271e47df1859e41cc82cf317c18ed91c78d24
guesung <gueit214@naver.com>
2024-08-13 14:17:55 +0900

    refactor: 기존 window.open에서 Link태그로 수정한다

--- src/components/Header/Banner.tsx
+++ src/components/Header/Banner.tsx
@@ -1,23 +1,27 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { URL } from '@/constants';
 
 export default function Banner() {
   return (
-    <div className="flex h-62 items-center gap-10 bg-violet-50 p-10">
-      <Image width={40} height={40} src="/images/app_icon.png" alt="" className="rounded-10" />
+    <div className="flex items-center gap-10 p-10 h-62 bg-violet-50">
+      <Image
+        width={40}
+        height={40}
+        src="/images/app_icon.png"
+        alt="main icon"
+        className="rounded-10"
+      />
       <div className="flex-grow">
         <div id="subtitle-1">포즈피커 - 네컷사진 포즈추천</div>
         <div id="subtitle-3" className="text-brand">
           앱에서 더 많은 기능 이용하기
         </div>
       </div>
-      <button
-        className="rounded-30 bg-main-violet px-12 py-5 text-white"
-        onClick={() => window.open(URL.appstore)}
-      >
+      <Link href={URL.appstore} className="px-12 py-5 text-white rounded-30 bg-main-violet">
         열기
-      </button>
+      </Link>
     </div>
   );
 }
