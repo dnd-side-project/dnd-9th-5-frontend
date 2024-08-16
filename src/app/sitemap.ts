6293345dfc0bf42936baf35d93ecc1f0ccc962e3
seondal <sseondal@gmail.com>
2024-08-16 22:42:56 +0900

    🛠 fix :  도메인 만료 관련 배포 이슈 해결될 때까지 Sitemap 보류

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,22 +1,22 @@
-import { getPoseFeed } from '@/apis';
+// import { getPoseFeed } from '@/apis';
 import { URL } from '@/constants';
 
 export default async function sitemap() {
   const baseUrl = URL.site;
 
-  const {
-    filteredContents: { content },
-  } = await getPoseFeed(0, 0, '', 0);
-  const detailUrls = content.map(({ poseInfo }) => ({
-    url: `${baseUrl}/detail/${poseInfo.poseId}`,
-    lastModified: poseInfo.updatedAt,
-  }));
+  // const {
+  //   filteredContents: { content },
+  // } = await getPoseFeed(0, 0, '', 0);
+  // const detailUrls = content.map(({ poseInfo }) => ({
+  //   url: `${baseUrl}/detail/${poseInfo.poseId}`,
+  //   lastModified: poseInfo.updatedAt,
+  // }));
 
   return [
     { url: baseUrl, lastModified: new Date() },
     { url: `${baseUrl}/pick`, lastModified: new Date() },
     { url: `${baseUrl}/talk`, lastModified: new Date() },
     { url: `${baseUrl}/feed`, lastModified: new Date() },
-    ...detailUrls,
+    // ...detailUrls,
   ];
 }
