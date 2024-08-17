cecf3154287fd74562d86abe908f1ee74b106ad8
seondal <sseondal@gmail.com>
2024-08-18 05:47:57 +0900

    🛠 fix :  ga4 대응

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import './globals.css';
 
+import Script from 'next/script';
 import { Suspense } from 'react';
 
-import { Analytics } from '@/components/Analytics';
 import { OverlayProvider } from '@/components/Overlay/OverlayProvider';
+import { GA_ID } from '@/constants/env';
 import METADATA from '@/constants/meta';
 import QueryProvider from '@/provider/QueryProvider';
 import RecoilProvider from '@/provider/RecoilProvider';
@@ -15,11 +16,24 @@ export const metadata: Metadata = METADATA;
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive" // after page is interactive
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
+      </head>
       <body className="flex w-screen touch-none justify-center bg-slate-100">
         <div className="w-full max-w-440 overflow-scroll bg-white text-primary">
-          <Suspense>
-            <Analytics />
-          </Suspense>
           <QueryProvider>
             <RecoilProvider>
               <OverlayProvider>{children}</OverlayProvider>
