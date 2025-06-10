8a76e890d6085e37f6dc92ad0e2aafaad0f43381
seondal <sseondal@gmail.com>
2025-06-10 17:29:28 +0900

    🚫 inactivate : login

--- src/app/(Sub)/auth/AuthComponent.tsx
+++ src/app/(Sub)/auth/AuthComponent.tsx
@@ -15,22 +15,23 @@ export default function AuthComponent({ code }: AuthComponentProps) {
   const router = useRouter();
 
   useDidMount(async () => {
-    try {
-      const {
-        token: { accessToken },
-        email,
-        nickname,
-      } = await getRegister(code);
+    // try {
+    //   const {
+    //     token: { accessToken },
+    //     email,
+    //     nickname,
+    //   } = await getRegister(code);
 
-      setClientCookie(COOKIE_ACCESS_TOKEN, accessToken);
-      setClientCookie(COOKIE_EMAIL, email);
-      setClientCookie(COOKIE_NICKNAME, nickname);
+    //   setClientCookie(COOKIE_ACCESS_TOKEN, accessToken);
+    //   setClientCookie(COOKIE_EMAIL, email);
+    //   setClientCookie(COOKIE_NICKNAME, nickname);
 
-      alert(`로그인에 성공했어요!`);
-      router.push('/');
-    } catch (error) {
-      router.push('/');
-    }
+    //   alert(`로그인에 성공했어요!`);
+    //   router.push('/');
+    // } catch (error) {
+    //   router.push('/');
+    // }
+    alert('로그인 기능은 준비중이에요');
   });
 
   return null;
